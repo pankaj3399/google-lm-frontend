b3bdaffc9d56b07c73551376843ec237003090d7
Priyanshu-Senslyze <priyanshu.suman@senslyze.com>
2024-10-31 14:16:10 +0530

    Import bug fix

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import SidebarItem from "./ui/SidebarItem";
-import IntegrationPopup from "./ui/IntegrationPopup";
+import IntegrationPopup from './ui/IntegrationPopup'
 
 interface SidebarProps {}
 
