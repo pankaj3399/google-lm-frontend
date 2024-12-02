7349303ce8a6f88aab366962d369105b0385ed20
Priyanshu-Senslyze <priyanshu.suman@senslyze.com>
2024-12-02 20:36:20 +0530

    Removed console

--- src/components/WorkspaceSidebar.tsx
+++ src/components/WorkspaceSidebar.tsx
@@ -81,7 +81,6 @@ const WorkspaceSidebar: React.FC<WorkspaceSidebarProps> = ({
             const resp = await apiClient.get(
                 `${API_URL}/api/users/getAllSources/${workspaceId}`
             );
-            console.log(resp.data)
             setSource(resp.data);
         } catch (error) {
             if (axios.isAxiosError(error)) {
