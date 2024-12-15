190b66ce9fec716eef7af51761050bc5ddc53e58
DHONDGE YASHVARDHAN CHATURBHUJ <152969155+Yashvardhandhondge@users.noreply.github.com>
2024-12-15 21:12:11 +0530

    update

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,16 @@ import './index.css';
 import App from './App.tsx';
 import { ClerkProvider } from '@clerk/clerk-react';
 import { Toaster } from 'react-hot-toast';
-import ReactGA from 'react-ga4';
+// import ReactGA from 'react-ga4';
 
 const clerk_key = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-const measurementId = import.meta.env.VITE_API_MESUREMENT_ID;
+// const measurementId = import.meta.env.VITE_API_MESUREMENT_ID;
 
 if(!clerk_key){
   throw new Error('Missing Clerk Key');
 }
 
-ReactGA.initialize(measurementId);
+// ReactGA.initialize(measurementId);
 
 createRoot(document.getElementById('root')!).render(
     <ClerkProvider publishableKey={clerk_key} afterSignOutUrl="/">
